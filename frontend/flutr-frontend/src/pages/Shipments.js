b39c4ee74be6f07219af96d6e6b299f89b5949f1
amandakfriis <amandafriis18@gmail.com>
2024-09-23 16:44:20 -0500

    added id to shipment list

--- frontend/flutr-frontend/src/pages/Shipments.js
+++ frontend/flutr-frontend/src/pages/Shipments.js
@@ -5,27 +5,27 @@ import "../styles/shipmentsStyles.css";
 
 export default function Shipments(){
 
-    const shipmentList = [
-        { shipmentDate: "06/10/2024", arrivalDate: "06/13/2024", supplier: "HBW" },
-        { shipmentDate: "06/28/2024", arrivalDate: "06/30/2024", supplier: "TEH" },
-        { shipmentDate: "06/27/2024", arrivalDate: "06/30/2024", supplier: "EBN" },
-        { shipmentDate: "07/10/2024", arrivalDate: "07/13/2024", supplier: "ABC" },
-        { shipmentDate: "07/18/2024", arrivalDate: "07/21/2024", supplier: "XYZ" },
-        { shipmentDate: "07/22/2024", arrivalDate: "07/25/2024", supplier: "MNO" },
-        { shipmentDate: "07/26/2024", arrivalDate: "07/29/2024", supplier: "QRS" },
-        { shipmentDate: "06/10/2024", arrivalDate: "06/13/2024", supplier: "HBW" },
-        { shipmentDate: "06/28/2024", arrivalDate: "06/31/2024", supplier: "TEH" },
-        { shipmentDate: "06/27/2024", arrivalDate: "06/30/2024", supplier: "EBN" },
-        { shipmentDate: "07/10/2024", arrivalDate: "07/13/2024", supplier: "ABC" },
-        { shipmentDate: "07/18/2024", arrivalDate: "07/21/2024", supplier: "XYZ" },
-        { shipmentDate: "07/22/2024", arrivalDate: "07/25/2024", supplier: "MNO" },
-        { shipmentDate: "07/26/2024", arrivalDate: "07/29/2024", supplier: "QRS" },
-        { shipmentDate: "06/10/2024", arrivalDate: "06/13/2024", supplier: "HBW" },
-        { shipmentDate: "06/28/2024", arrivalDate: "06/31/2024", supplier: "TEH" },
-        { shipmentDate: "06/27/2024", arrivalDate: "06/30/2024", supplier: "EBN" },
-        { shipmentDate: "07/10/2024", arrivalDate: "07/13/2024", supplier: "ABC" },
-        { shipmentDate: "07/18/2024", arrivalDate: "07/21/2024", supplier: "XYZ" }
-    ];
+const shipmentList = [
+    { id: 1, shipmentDate: "06/10/2024", arrivalDate: "06/13/2024", supplier: "HBW" },
+    { id: 2, shipmentDate: "06/28/2024", arrivalDate: "06/30/2024", supplier: "TEH" },
+    { id: 3, shipmentDate: "06/27/2024", arrivalDate: "06/30/2024", supplier: "EBN" },
+    { id: 4, shipmentDate: "07/10/2024", arrivalDate: "07/13/2024", supplier: "ABC" },
+    { id: 5, shipmentDate: "07/18/2024", arrivalDate: "07/21/2024", supplier: "XYZ" },
+    { id: 6, shipmentDate: "07/22/2024", arrivalDate: "07/25/2024", supplier: "MNO" },
+    { id: 7, shipmentDate: "07/26/2024", arrivalDate: "07/29/2024", supplier: "QRS" },
+    { id: 8, shipmentDate: "06/10/2024", arrivalDate: "06/13/2024", supplier: "HBW" },
+    { id: 9, shipmentDate: "06/28/2024", arrivalDate: "06/31/2024", supplier: "TEH" },
+    { id: 10, shipmentDate: "06/27/2024", arrivalDate: "06/30/2024", supplier: "EBN" },
+    { id: 11, shipmentDate: "07/10/2024", arrivalDate: "07/13/2024", supplier: "ABC" },
+    { id: 12, shipmentDate: "07/18/2024", arrivalDate: "07/21/2024", supplier: "XYZ" },
+    { id: 13, shipmentDate: "07/22/2024", arrivalDate: "07/25/2024", supplier: "MNO" },
+    { id: 14, shipmentDate: "07/26/2024", arrivalDate: "07/29/2024", supplier: "QRS" },
+    { id: 15, shipmentDate: "06/10/2024", arrivalDate: "06/13/2024", supplier: "HBW" },
+    { id: 16, shipmentDate: "06/28/2024", arrivalDate: "06/31/2024", supplier: "TEH" },
+    { id: 17, shipmentDate: "06/27/2024", arrivalDate: "06/30/2024", supplier: "EBN" },
+    { id: 18, shipmentDate: "07/10/2024", arrivalDate: "07/13/2024", supplier: "ABC" },
+    { id: 19, shipmentDate: "07/18/2024", arrivalDate: "07/21/2024", supplier: "XYZ" }
+];
 
     const[currentPage, setCurrentPage] = useState(1);
     const rowsPerPage = 8;
@@ -55,7 +55,7 @@ export default function Shipments(){
                 <table className="table">
                     <thead>
                         <tr>
-                            <th>Shipment Date</th>
+                            <th>Shipment Dates</th>
                             <th>Arrival Date</th>
                             <th>Supplier</th>
                             <th style={{width:'128px'}}></th>
@@ -67,7 +67,9 @@ export default function Shipments(){
                                 <td>{shipmentList.shipmentDate}</td>
                                 <td>{shipmentList.arrivalDate}</td>
                                 <td>{shipmentList.supplier}</td>
-                                <td style={{backgroundColor:"#469FCE", color:"#E1EFFE"}}>edit</td>
+
+                                {/* TODO: Edit button opens 'add shipment' page with shipment info auto-populated */}
+                                <td style={{backgroundColor:"#469FCE", color:"#E1EFFE"}}>edit</td> 
                             </tr>
                         ))}
                     </tbody>
