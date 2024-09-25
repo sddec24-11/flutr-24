40f97b808fa77f0c703cad0609d01fd01733c21e
alexthebrown <99758041+alexthebrown@users.noreply.github.com>
2024-09-25 16:21:03 -0500

    Stuff

--- frontend/flutr-frontend/src/components/ButterflyEditCard.jsx
+++ frontend/flutr-frontend/src/components/ButterflyEditCard.jsx
@@ -2,24 +2,46 @@ import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 
-export default function ButterflyEditCard({butterfly, changeList}){
-    const [common, setCommon] = useState(butterfly.common_name);
-    const [lifespan, setLifespan] = useState(butterfly.lifespan);
-    const [changePoint, setChangePoint] = useState(-1);
+export default function ButterflyEditCard({butterfly, changeList, index, editPoints}){
+    const [common, setCommon] = useState(() => editPoints[index] != -1 ? butterfly.common_name : changeList[editPoints[index]].common_name);
+    const [lifespan, setLifespan] = useState(() => editPoints[index] != -1 ? butterfly.lifespan : changeList[editPoints[index]].lifespan);
+    // const [changePoint, setChangePoint] = useState(editPoints[]);
 
+    // const addToChangeList = () => {
+    //     if(changePoint === -1){
+    //         setChangePoint(changeList.length);
+    //         changeList.push(
+    //             {
+    //                 id: butterfly.id,
+    //                 common_name: common,
+    //                 lifespan: lifespan
+    //             }
+    //         );
+    //         editPoints[index] = changePoint;
+    //     }
+    //     else{
+    //         changeList[changePoint] = {
+    //             id: butterfly.id,
+    //             common_name: common,
+    //             lifespan: lifespan
+    //         }
+    //     }
+    //     console.log(changeList);
+    // }
     const addToChangeList = () => {
-        if(changePoint === -1){
-            setChangePoint(changeList.length)
+        if(editPoints[index] === -1){
+            // setChangePoint(changeList.length);
+            editPoints[index] = changeList.length;
             changeList.push(
                 {
                     id: butterfly.id,
                     common_name: common,
                     lifespan: lifespan
                 }
-            )
+            );
         }
         else{
-            changeList[changePoint] = {
+            changeList[editPoints[index]] = {
                 id: butterfly.id,
                 common_name: common,
                 lifespan: lifespan
@@ -40,7 +62,7 @@ export default function ButterflyEditCard({butterfly, changeList}){
     }
     
     return(
-        <Col key={butterfly.id}>
+        <Col key={index}>
                 <Card style={{  backgroundColor: "#E1EFFE", borderRadius: '15px', border: '4px solid #8ABCD7', alignItems: 'center',justifyContent: 'center', margin: '10px'}}>
                     <Card.Img variant="top" style={{width: '100%', padding: '0', borderRadius: '11px'}} src={require(`../images/${butterfly.image}`)} />
                     <Card.Body>
--- frontend/flutr-frontend/src/pages/EditButterflies.js
+++ frontend/flutr-frontend/src/pages/EditButterflies.js
@@ -2,6 +2,7 @@ import Navbar from "../components/navbar"
 import ButterflyEditCard from "../components/ButterflyEditCard";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
 import React, {useState} from 'react';
 
 const butterflies = [
@@ -21,26 +22,54 @@ const butterflies = [
     },
     {
         id: 2,
-        sci_name: "Bi",
+        sci_name: "Biterfly",
         common_name: "The second one",
         lifespan: 2,
         image: "reiman-logo.png",
     },
 ];
 
-export default function EditButterflies(){
+export default function EditButterflies({isMasterUser}){
     const [changeList, setChangeList] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
+    const [editPoints, setEditPoints] = useState([]);
+
+    const handleChangeSearch = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+    };
+
+    const handleAdd = (e) => {
+        e.preventDefault();
+        console.log("Redirect or open add butterfly form")
+        // Redirect To Add Butterfly Form
+    }
 
     return (
         <div>
             <Navbar/>
+            <h3>Edit Butterflies</h3>
             <div>
+                <div>
+                    <Container>
+                        <Row>
+                            <Col style={{width: '100%', margin: 'auto'}} onChange={handleChangeSearch}><input></input></Col>
+                            {isMasterUser && <Col style={{width: '100%', margin: 'auto'}}>
+                                <button onClick={handleAdd}>Add Butterfly</button>
+                            </Col>}
+                        </Row>
+                    </Container>
+                </div>
                 <Container>
                     <Row xs={1} sm={2} md={2} lg={3}>
-                        {butterflies.map((r) => {
-                            return(
-                                <ButterflyEditCard butterfly={r} changeList={changeList} />
-                            )
+                        {butterflies.map((r, index) => {
+                            
+                            if(r.sci_name.toLowerCase().includes(searchInput.toLowerCase())){
+                                return(
+                                    <ButterflyEditCard butterfly={r} changeList={changeList}  index={index} editPoints={editPoints}/>
+                                )
+                            }
+                            
                         })}
                     </Row>
                 </Container>
