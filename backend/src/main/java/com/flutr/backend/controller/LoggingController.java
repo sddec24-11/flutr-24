79fd4c1a0ef327bd9576248c067f526c073556a2
muralip2020 <muralip@iastate.edu>
2024-11-16 07:39:29 -0600

    logs endpoint url fix

--- backend/src/main/java/com/flutr/backend/controller/LoggingController.java
+++ backend/src/main/java/com/flutr/backend/controller/LoggingController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.flutr.backend.dto.Response;
 import com.flutr.backend.model.LogEntry;
 import com.flutr.backend.service.LoggingService;
 
+@RestController
+@RequestMapping("/logs")
 public class LoggingController {
     @Autowired
     private LoggingService loggingService;
