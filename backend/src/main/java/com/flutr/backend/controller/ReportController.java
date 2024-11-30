d885219487faea6316c1697668858984bdf99e39
muralip2020 <muralip@iastate.edu>
2024-11-29 21:45:13 -0600

    import historical csv

--- backend/src/main/java/com/flutr/backend/controller/ReportController.java
+++ backend/src/main/java/com/flutr/backend/controller/ReportController.java
@@ -8,11 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.flutr.backend.dto.Response;
+import com.flutr.backend.dto.Response.ErrorDetails;
 import com.flutr.backend.service.ReportService;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -36,6 +39,24 @@ public class ReportController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new Response<>(false, null, new Response.ErrorDetails(500, "Internal server error:" + e)));
+        }
+    }
+
+    @PostMapping("/import")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<Response<List<String>>> importShipments(@RequestParam("file") MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                return ResponseEntity.badRequest().body(new Response<>(false, null, new ErrorDetails(400, "No file uploaded")));
+            }
+            List<String> errors = reportService.importShipmentsFromCsv(file);
+            if (!errors.isEmpty()) {
+                return ResponseEntity.badRequest().body(new Response<>(false, errors, new ErrorDetails(400, "Errors encountered during file processing")));
+            }
+            return ResponseEntity.ok(new Response<>(true, null, new ErrorDetails(200, "File processed successfully")));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new Response<>(false, null, new ErrorDetails(500, "Failed to process file: " + e.getMessage())));
         }
     }
 }
